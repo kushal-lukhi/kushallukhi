50c2653cd268ae39783cff22e5ed7e5440c4ee7d
kushal-lukhi <131652552+kushal-lukhi@users.noreply.github.com>
2023-05-09 01:22:50 -0700

    Add files via upload

---
+++ EVEN_ODD.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int N;
+clrscr();
+
+printf("enter the number:");
+scanf("%d",&N);
+
+
+(N%2==0)?printf("number is even"):printf("number is odd");
+
+getch();
+}
